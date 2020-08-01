39063986f4e47578f48906cd006a4730e333e313
Mohana Bansal <mohanabansal10@gmail.com>
2020-08-01 17:41:01 -0400

    updated to show login and logout navs

--- client/components/home/index.js
+++ client/components/home/index.js
@@ -3,32 +3,37 @@ import {connect} from 'react-redux'
 import {getMe} from '../../reducer/user'
 import NavigationBar from '../navigationbar'
 import Recipe from '../recipe'
+import './index.css'
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
         <NavigationBar />
-        {this.props.currentUser ? (
-          <h1>{`Welcome ${this.props.currentUser.firstName}`}</h1>
-        ) : (
-          <h1>Welcome</h1>
-        )}
-        <Recipe />
+        <div className="content">
+          {this.props.currentUser &&
+          Object.keys(this.props.currentUser).length !== 0 ? (
+            <h1 className="welcome">{`Welcome ${
+              this.props.currentUser.firstName
+            }`}</h1>
+          ) : (
+            <h1 className="welcome">Welcome</h1>
+          )}
+          <Recipe />
+        </div>
       </div>
     )
   }
 }
 
 const mapStateToProps = state => {
-  // console.log('checking user in global state', state.user.currentUser)
   return {
     currentUser: state.user.currentUser
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  getCurrentUser: () => dispatch(getMe())
-})
+// const mapDispatchToProps = (dispatch) => ({
+//   getCurrentUser: () => dispatch(getMe()),
+// })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default connect(mapStateToProps, null)(Home)
--- client/components/navigationbar/index.js
+++ client/components/navigationbar/index.js
@@ -1,18 +1,37 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {NavLink} from 'react-router-dom'
+import {logoutUserFromServer} from '../../reducer/user'
+import './index.css'
 
 class NavigationBar extends Component {
   render() {
+    console.log('Nav bar', this.props.currentUser)
     return (
-      <div>
+      <div className="navbar">
         <ul>
           <NavLink to="/signup">Signup</NavLink>
-          <NavLink to="/login">Login</NavLink>
+          {this.props.currentUser &&
+          Object.keys(this.props.currentUser).length !== 0 ? (
+            <NavLink to="/" onClick={() => this.props.logout()}>
+              Logout
+            </NavLink>
+          ) : (
+            <NavLink to="/login">Login</NavLink>
+          )}
+          {/* <NavLink to="/login">{this.props.currentUser && Object.keys(this.props.currentUser).length!==0 ? 'Logout' : 'Login'}</NavLink> */}
         </ul>
       </div>
     )
   }
 }
 
-export default connect(null, null)(NavigationBar)
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logoutUserFromServer())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavigationBar)
